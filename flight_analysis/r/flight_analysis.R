64127f2aac2224acd1ab0e1e62b04f3f7185d063
Lucas Ketzer <lucasketzerdosreis@gmail.com>
2022-05-10 15:11:24 -0300

    feat: answering question about airlines groups

--- flight_analysis/r/flight_analysis.R
+++ flight_analysis/r/flight_analysis.R
@@ -1,9 +1,8 @@
 # importing libraries
 library(nycflights13)
 library(tidyverse)
-library(ggridges)
 library(tidylog)
-library(lubridate)
+library(Information)
 
 #checking nas on the delays
 flights %>%
@@ -30,6 +29,7 @@ flights %>%
     ) %>%
     count()
 
+### ---- QUESTION 1 ---- ###
 
 # less than 1% of delays can be properly treated,
 # best to follow without them - creating function
@@ -47,12 +47,17 @@ create_summary <- function(df, var) {
                         )
 }
 
+# creating delay and day of flight variables
+# for dataframe
+flights <- flights %>%
+            mutate(
+                total_delay = dep_delay + arr_delay,
+                flight_day_month = lubridate::ymd(str_sub(time_hour, 1, 10)),
+                is_delayed = ifelse(total_delay > 0, 1, 0)
+            )
+
 # creating summary for each day of the year
 delay_per_day <- flights %>%
-                    mutate(
-                        total_delay = dep_delay + arr_delay,
-                        flight_day_month = ymd(str_sub(time_hour, 1, 10)),
-                    ) %>%
                     filter(!is.na(total_delay)) %>%
                     group_by(flight_day_month) %>%
                     create_summary(total_delay) %>%
@@ -113,7 +118,6 @@ delay_per_day %>%
     ) %>%
     view(.)
 
-
 delay_per_day %>%
     ungroup() %>%
     group_by(week) %>%
@@ -126,6 +130,13 @@ delay_per_day %>%
     ) %>%
     view(.)
 
+# counting flights, delays and early arrivals per month
+flights %>%
+    mutate(month = strftime(flight_day_month, "%m")) %>%
+    select(month, is_delayed) %>%
+    table(.) %>%
+    addmargins(.)
+
 # some weeks and days concentrate more delays
 # than others. lastly, analyzing percentiles 25 and 50
 delay_per_day %>%
@@ -135,3 +146,77 @@ delay_per_day %>%
 delay_per_day %>%
     pull(p50) %>%
     quantile(., probs = seq(0, 1, by = 0.1))
+
+### ---- QUESTION 2 ---- ###
+
+# checking if there is a particular airline that has a 
+# higher percentage of delays compared to the total
+
+delays_per_airline <- flights %>%
+                            filter(!is.na(total_delay)) %>%
+                            inner_join(airlines, by = "carrier") %>%
+                            select(name, is_delayed) %>%
+                            table(.)
+
+prop_delays_airline <- delays_per_airline %>%
+                        prop.table(margin = 1) %>%
+                        as.data.frame(.) %>%
+                        pivot_wider(names_from = is_delayed, values_from = Freq) %>%
+                        rename(N = `0`, Y = `1`) %>%
+                        mutate(early_arrivals_delays_ratio = N / Y) %>%
+                        arrange(., early_arrivals_delays_ratio) %>%
+                        rbind(
+                            flights %>%
+                                select(is_delayed) %>%
+                                table(.) %>%
+                                prop.table(.) %>%
+                                as.data.frame(.) %>%
+                                mutate(name = "Total") %>%
+                                pivot_wider(., names_from = ., values_from = Freq) %>%
+                                rename(N = `0`, Y = `1`) %>%
+                                mutate(early_arrivals_delays_ratio = N / Y)
+                        )
+
+
+# creating airline groups based on distribution
+# of delays and non delays
+airline_groups <- flights %>%
+                    inner_join(airlines, by = "carrier") %>%
+                    mutate(group = case_when(
+                        name %in% c("Frontier Airlines Inc.", "AirTran Airways Corporation") ~ 1,
+                        name %in% c("Southwest Airlines Co.", "ExpressJet Airlines Inc.", "Mesa Airlines Inc.") ~ 2,
+                        name %in% c("JetBlue Airways", "Envoy Air", "United Air Lines Inc.", "Endeavor Air Inc.") ~ 3,
+                        name %in% c("American Airlines Inc.", "US Airways Inc.", "Delta Air Lines Inc", "Delta Air Lines Inc.", "Virgin America") ~ 4,
+                        name %in% c("SkyWest Airlines Inc.", "Alaska Airlines Inc.") ~ 5,
+                        TRUE ~ 6
+                    )) 
+
+airline_groups %>%
+    select(group, is_delayed) %>%
+    table(.) %>%
+    prop.table(margin = 1) %>%
+    as.data.frame(.) %>%
+    pivot_wider(names_from = is_delayed, values_from = Freq) %>%
+    rename(N = `0`, Y = `1`) %>%
+    mutate(early_arrivals_delays_ratio = N / Y) %>% 
+    arrange(., early_arrivals_delays_ratio) %>%
+    rbind(
+        flights %>%
+            select(is_delayed) %>%
+            table(.) %>%
+            prop.table(.) %>%
+            as.data.frame(.) %>%
+            mutate(group = "Total") %>%
+            pivot_wider(., names_from = ., values_from = Freq) %>%
+            rename(N = `0`, Y = `1`) %>%
+            mutate(early_arrivals_delays_ratio = N / Y)
+    )
+
+# calculating iv statistic to verify
+# if variables can discriminate target variable
+airline_groups %>%
+    select(group, is_delayed) %>%
+    filter(!is.na(is_delayed)) %>%
+    create_infotables(., y = "is_delayed", bins = 6)
+  
+### ---- QUESTION 3 ---- ###
